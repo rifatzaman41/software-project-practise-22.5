3157f44e464f34f66f921919b8a807a9a7c85d84
rifatzaman41 <127728672+rifatzaman41@users.noreply.github.com>
2024-07-16 22:49:20 +0600

    mamar bank2 codes

---
+++ transactions/constants.py
@@ -0,0 +1,15 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+SEND_MONEY = 5
+RECEIVE_MONEY = 6
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+        (SEND_MONEY, 'Send money'),
+    (RECEIVE_MONEY, 'Receive money'),
+
+)
